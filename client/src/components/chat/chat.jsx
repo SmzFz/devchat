5add39a7329a8a46fe1f865caa58b355eafac9dc
SmzFz <felipesalmazo123@gmail.com>
2023-09-21 10:19:56 -0300

    adicionando comentários no codigo

--- client/src/components/chat/chat.jsx
+++ client/src/components/chat/chat.jsx
@@ -1,54 +1,76 @@
+// Importação de estilos do arquivo chat.module.css
 import style from "./chat.module.css";
+
+// Importação de componentes e hooks necessários
 import SendIcon from "@mui/icons-material/Send";
 import { Input } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 
-
-
+// Definição do componente Chat
 const Chat = ({ socket }) => {
-  const messageRef = useRef();
-  const bottomRef = useRef();
+  // Refs para elementos do DOM
+  const messageRef = useRef(); // Ref para a entrada de mensagem
+  const bottomRef = useRef(); // Ref para o elemento inferior para rolagem automática
+
+  // Estado para armazenar as mensagens
   const [messageBox, setMessageBox] = useState([]);
 
+  // Efeito que foca na entrada de mensagem quando o componente monta
   useEffect(() => {
     messageRef.current.focus();
+
+    // Listener para receber mensagens do socket
     socket.on("receive_message", (data) => {
-      setMessageBox((current) => 
-        [...current, data]);
-      });
+      // Atualiza o estado das mensagens com a nova mensagem recebida
+      setMessageBox((current) => [...current, data]);
+    });
+
+    // Função de cleanup que remove o listener quando o componente desmonta
     return () => socket.off("receive_message");
   }, [socket]);
 
+  // Efeito para rolar automaticamente para a última mensagem quando há uma nova mensagem
   useEffect(() => {
     bottomRef.current.scrollIntoView({ behavior: "smooth" });
   }, [messageBox]);
 
+  // Função para enviar uma mensagem
   const messageSubmit = () => {
     const message = messageRef.current.value;
-    if (!message.trim()) return;
+    if (!message.trim()) return; // Ignora mensagens em branco
 
+    // Emite a mensagem via socket
     socket.emit("message", message);
 
+    // Limpa a entrada de mensagem e coloca o foco de volta nela
     clearInput();
     messageRef.current.focus();
   };
+
+  // Função para limpar o conteúdo da entrada de mensagem
   const clearInput = () => {
     messageRef.current.value = "";
   };
+
+  // Manipula o evento de pressionar a tecla Enter na entrada de mensagem
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       messageSubmit();
     }
   };
 
+  // Renderização do componente Chat
   return (
     <>
       <div className={style["chat-container"]}>
         <div className={style["chat-body"]}>
+          {/* Mapeia e renderiza as mensagens */}
           {messageBox.map((message, index) => (
             <div
-                className={`${style["message-container"]} 
-                ${message.authorID === socket.id && style["message-mine"]}`}
+              className={`${style["message-container"]} ${
+                // Adiciona a classe "message-mine" se a mensagem for do próprio usuário
+                message.authorID === socket.id && style["message-mine"]
+              }`}
               key={index}
             >
               <div className={style["message-author"]}>
@@ -57,9 +79,10 @@ const Chat = ({ socket }) => {
               <div className={style["message-text"]}>{message.text}</div>
             </div>
           ))}
-          <div ref={bottomRef} />
+          <div ref={bottomRef} /> {/* Elemento para rolagem automática */}
         </div>
 
+        {/* Componentes para a entrada de mensagem e envio */}
         <div className={style["chat-footer"]}>
           <Input
             inputRef={messageRef}
@@ -78,4 +101,5 @@ const Chat = ({ socket }) => {
   );
 };
 
-export default Chat;
+export default Chat; // Exporta o componente Chat
+
--- client/src/components/join/join.jsx
+++ client/src/components/join/join.jsx
@@ -1,47 +1,55 @@
-import style from "./join.module.css";
-import { Input, Button } from "@mui/material";
-import devLogo from "../../assets/devChat.png";
-import io from "socket.io-client";
-import { useEffect, useRef } from "react";
+// Importação de módulos e componentes necessários
+import style from "./join.module.css"; // Importa estilos CSS específicos para este componente
+import { Input, Button } from "@mui/material"; // Importa componentes de entrada e botão do Material UI
+import devLogo from "../../assets/devChat.png"; // Importa uma imagem do logo
+import io from "socket.io-client"; // Importa a biblioteca socket.io-client para comunicação via WebSocket
+import { useEffect, useRef } from "react"; // Importa hooks do React (useEffect e useRef)
 
+// Definição do componente Join
 const Join = ({ state, handShake }) => {
+  // Cria uma ref para o input de username
   const usernameRef = useRef();
 
+  // Efeito que faz o foco ser direcionado para o input de username ao carregar o componente
   useEffect(() => {
     usernameRef.current.focus();
   });
+
+  // Função para lidar com o envio do nome de usuário
   const submit = async () => {
     const username = usernameRef.current.value;
-    if (!username.trim()) return;
-    const socket = await io.connect("http://localhost:3001");
-    socket.emit("set_username", username);
-    //console.log(`Bem-vindo ${username}`)
-    handShake(socket);
-    state(true);
+    if (!username.trim()) return; // Verifica se o nome de usuário não está vazio ou apenas com espaços em branco
+    const socket = await io.connect("http://localhost:3001"); // Conecta-se ao servidor WebSocket
+    socket.emit("set_username", username); // Emite um evento "set_username" para configurar o nome de usuário no servidor
+    handShake(socket); // Chama a função handShake passando o socket como argumento
+    state(true); // Chama a função state passando true como argumento
   };
 
+  // Função para lidar com a tecla Enter pressionada
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
-      submit();
+      submit(); // Chama a função submit se a tecla Enter for pressionada
     }
   };
+
+  // Renderização do componente
   return (
     <>
       <div className={style["dev-logo"]}>
-        <img src={devLogo} alt="" />
+        <img src={devLogo} alt="" /> {/* Exibe o logo do devChat */}
       </div>
       <div className={style["join-container"]}>
-        <h2>Bem-vindo ao devChat</h2>
+        <h2>Bem-vindo ao devChat</h2> {/* Título do componente */}
 
         <Input
           inputRef={usernameRef}
           placeholder="Nome de usuário..."
-          onKeyDown={handleKeyPress}
+          onKeyDown={handleKeyPress} // Chama a função handleKeyPress quando uma tecla é pressionada
         />
         <Button
           sx={{ mt: 2, mb: 2 }}
           variant="contained"
-          onClick={() => submit()}
+          onClick={() => submit()} // Chama a função submit ao clicar no botão
         >
           {" "}
           Entrar
@@ -51,4 +59,4 @@ const Join = ({ state, handShake }) => {
   );
 };
 
-export default Join;
+export default Join; // Exporta o componente Join para uso em outros lugares do aplicativo
